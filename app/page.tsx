3ae5f9d996f52908a07d7995631e47167aa257d1
mccantsquincy <quincymccants@gmail.com>
2024-01-07 10:48:17 -0800

    installed shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,20 @@
 import Image from 'next/image'
 
+"use client"
+
 export default function Home() {
    // all logic here
-
+   
+const handleClick = () => {
+  console.log("clicked")
+}
 
   return (
     <div>
       {/* css html: tsx  */}
-      SWE Club
+      Software engineering club
+      
+      <button onClick={handleClick}>click me </button>
     </div>
   )
 }
