3d3febf247e0e6f430f0ad3b748515f99852cc27
Luis <104567400+Surdavow@users.noreply.github.com>
2024-01-08 15:05:05 -0800

    Create the card component in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,15 @@ import Image from 'next/image'
 "use client"
 
 import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
 export default function Home() {
    // all logic here
    
@@ -18,6 +27,22 @@ const handleClick = () => {
       <br /><br />
       <Button onClick={handleClick}>Click Me</Button>
       </center>
+
+      <div className='p-6 pt-4'>
+      <Card>
+        <CardHeader>
+          <CardTitle>Main Page</CardTitle>
+          <CardDescription>The home page of the Software Engineering Club Website.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Test page work in progress, still incomplete.
+          </p>
+        </CardContent>
+        <CardFooter>
+          <p>Footer</p>
+        </CardFooter>
+      </Card>
+      </div>
     </div>    
   )
 }
