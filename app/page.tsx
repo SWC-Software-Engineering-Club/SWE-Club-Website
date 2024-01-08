a77c823b409a13d29a2989079f4b6b9f9cd9da44
Luis <104567400+Surdavow@users.noreply.github.com>
2024-01-07 20:44:41 -0800

    Update the links in the dropdown

--- app/page.tsx
+++ app/page.tsx
@@ -34,55 +34,23 @@ const handleClick = () => {
       <br /><br />      
       <Button onClick={handleClick}>Click Me</Button>
       </center>
-
       <div className="absolute top-2 right-2">
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button variant="outline">Menu</Button>
         </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-56 ">
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
-          <DropdownMenuSeparator />
+        <DropdownMenuContent className="w-56 ">    
           <DropdownMenuGroup>
             <DropdownMenuItem>
-              Profile
+              Club Directory
             </DropdownMenuItem>
             <DropdownMenuItem>
-              Billing
+              Signup
             </DropdownMenuItem>
             <DropdownMenuItem>
-              Settings
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              Keyboard shortcuts
+              Blogs
             </DropdownMenuItem>
           </DropdownMenuGroup>
-          <DropdownMenuSeparator />
-          <DropdownMenuGroup>
-            <DropdownMenuItem>Team</DropdownMenuItem>
-            <DropdownMenuSub>
-              <DropdownMenuSubTrigger>Invite users</DropdownMenuSubTrigger>
-              <DropdownMenuPortal>
-                <DropdownMenuSubContent>
-                  <DropdownMenuItem>Email</DropdownMenuItem>
-                  <DropdownMenuItem>Message</DropdownMenuItem>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuItem>More...</DropdownMenuItem>
-                </DropdownMenuSubContent>
-              </DropdownMenuPortal>
-            </DropdownMenuSub>
-            <DropdownMenuItem>
-              New Team
-            </DropdownMenuItem>
-          </DropdownMenuGroup>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>GitHub</DropdownMenuItem>
-          <DropdownMenuItem>Support</DropdownMenuItem>
-          <DropdownMenuItem disabled>API</DropdownMenuItem>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>
-            Log out
-          </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>      
       </div>
