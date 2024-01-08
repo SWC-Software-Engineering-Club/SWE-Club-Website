48717b51aa9ca1156372f99bb9599934dd2f2a2d
Luis <104567400+Surdavow@users.noreply.github.com>
2024-01-07 21:24:25 -0800

    Add dropdown to nav component

--- app/page.tsx
+++ app/page.tsx
@@ -3,22 +3,6 @@ import Image from 'next/image'
 "use client"
 
 import { Button } from '@/components/ui/button'
-
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuPortal,
-  DropdownMenuSeparator,
-  DropdownMenuShortcut,
-  DropdownMenuSub,
-  DropdownMenuSubContent,
-  DropdownMenuSubTrigger,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-
 export default function Home() {
    // all logic here
    
@@ -27,33 +11,13 @@ const handleClick = () => {
 }
 
   return (
-    <div className='p-6 pt-20'>
+    <div className='p-6 pt-28'>
       {/* css html: tsx  */}
       <center>
       Welcome to the Software Engineering Club main page!
-      <br /><br />      
+      <br /><br />
       <Button onClick={handleClick}>Click Me</Button>
       </center>
-      <div className="absolute top-2 right-2">
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="outline">Menu</Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-56 ">    
-          <DropdownMenuGroup>
-            <DropdownMenuItem>
-              Club Directory
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              Signup
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              Blogs
-            </DropdownMenuItem>
-          </DropdownMenuGroup>
-        </DropdownMenuContent>
-      </DropdownMenu>      
-      </div>
     </div>    
   )
 }
--- components/nav.tsx
+++ components/nav.tsx
@@ -1,10 +1,43 @@
+import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
 const Navbar = () => {
     return ( 
       <nav className="w-full p-6 flex fixed justify-between border-b shadow-sm">
        <div>
         SWE Club
        </div>
-       {/* <div></div> */}
+       <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline">Menu</Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56 ">    
+          <DropdownMenuGroup>
+            <DropdownMenuItem>
+              Club Directory
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              Signup
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              Blogs
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>      
       </nav>
     );
 }
