6ab51b240add94dc7bb18147a54d98ed77494f34
mccantsquincy <quincymccants@gmail.com>
2024-01-08 21:35:17 -0800

    added dark mode toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,36 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Navbar from '@/components/nav'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/nav";
+import { ThemeProvider } from "@/components/theme-provider";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'SWC Software Engineering Club',
-  description: 'The page for Southwestern College\'s Software Engineering Club',
-}
+  title: "SWC Software Engineering Club",
+  description: "The page for Southwestern College's Software Engineering Club",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className='max-w-10xl mx-auto'>
-          <Navbar />
-          {children}
-        </main>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <main className="max-w-10xl mx-auto">
+            <Navbar />
+            {children}
+          </main>
+        </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
--- components/nav.tsx
+++ components/nav.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -7,24 +9,45 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import Link from "next/link";
+import { useTheme } from "next-themes";
+import { ModeToggle } from "./ui/mode-toggle";
+
+
 
 const Navbar = () => {
+  const { theme, setTheme } = useTheme();
+
+  const toggleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+  
   return (
-    <nav className="w-full p-6 flex fixed justify-between border-b shadow-sm">
+    <nav className="w-full p-6 flex fixed justify-between items-center border-b shadow-sm">
       <div>SWE Club</div>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="outline">Menu</Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-56 ">
-          <DropdownMenuGroup>
-          <DropdownMenuItem><Link href="/">Home</Link></DropdownMenuItem>            
-            <DropdownMenuItem><Link href="/club-directory">Club Directory</Link></DropdownMenuItem>
-            <DropdownMenuItem><Link href="/signup">Signup</Link></DropdownMenuItem>
-            <DropdownMenuItem><Link href="/blogs">Blogs</Link></DropdownMenuItem>
-          </DropdownMenuGroup>
-        </DropdownMenuContent>
-      </DropdownMenu>
+      <div className="flex items-center gap-4">
+        <ModeToggle />
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline">Menu</Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-56">
+            <DropdownMenuGroup>
+              <DropdownMenuItem>
+                <Link href="/">Home</Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Link href="/club-directory">Club Directory</Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Link href="/signup">Signup</Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Link href="/blogs">Blogs</Link>
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
     </nav>
   );
 };
