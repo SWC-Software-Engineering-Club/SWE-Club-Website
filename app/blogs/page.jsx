84c7acbf8b085577dd946f8b60b5876a2645c4c4
Luis <104567400+Surdavow@users.noreply.github.com>
2024-01-08 18:48:49 -0800

    Create website routes
    We just need to make these pages accessible from the navigation bar menu button.

---
+++ app/blogs/page.jsx
@@ -0,0 +1,10 @@
+export default function Blogs()
+{
+  return (
+    <div className='p-0 pt-28'>
+      <center>
+      Blogs page
+      </center>
+    </div>    
+  )
+}
---
+++ app/club-directory/page.jsx
@@ -0,0 +1,10 @@
+export default function ClubDirectory()
+{
+  return (
+    <div className='p-0 pt-28'>
+      <center>
+      Club Directory page
+      </center>
+    </div>    
+  )
+}
