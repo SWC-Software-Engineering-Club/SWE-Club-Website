89a82c936c092dae92e64e64992ff4d63697b02a
Luis <104567400+Surdavow@users.noreply.github.com>
2024-01-12 22:07:29 -0800

    Actual fix
    This should remove that terminal popup now, I had to adjust the hero some more, but that popup saying the props are not recommend or whatever the issue was should be solved.

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,18 +1,19 @@
-import Image from "next/legacy/image";
+import Image from "next/image";
 
 const Hero = () => {
-    return ( 
-        <div className="w-full m-auto overflow-hidden">
+  return (
+    <div className="w-full m-auto overflow-hidden" style={{ width: "100%", maxWidth: "4000px" }}>
+      <div className="relative" style={{ width: "100%", paddingTop: "50%" }}>
         <Image
           src="/reference.png"
           alt="work illustration"
-          layout="responsive"
-          objectFit="cover"
-          width={2000} // Use a large width value to fill the container
-          height={1000} // Set a fixed height to maintain the aspect ratio
+          className="absolute inset-0 w-full h-full"
+          width={4000}
+          height={2000}
         />
       </div>
-     );
-}
- 
+    </div>
+  );
+};
+
 export default Hero;
