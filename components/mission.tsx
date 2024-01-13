9bd6deee15774283387c4c6069ad709b36c79b78
Luis <104567400+Surdavow@users.noreply.github.com>
2024-01-12 21:21:28 -0800

    Implement dynamic light/dark code

--- components/mission.tsx
+++ components/mission.tsx
@@ -1,14 +1,22 @@
 const Mission = () => {
   return ( 
     <div className="max-w-3xl space-y-7 flex flex-col items-center mb-20 mt-20">
-      <img src="/mission-icon_dark.svg" alt="Mission Icon" style={{ width: '10%' }} />      
-      <h1 className="text-3xl sm:text-5xl md:text-3xl font-bold">
+      <img src="/icon-dark.svg" alt="Mission Icon" style={{ width: '10%' }} /> 
+      {/*<img className="light:block" src="/icon-light.svg" alt="Mission Icon" style={{ width: '10%' }} />*/}
+
+      <h3 className="text-3xl sm:text-5xl md:text-3xl font-bold">
         Our Mission
-      </h1>
-      <h3 className="text-neutral-400 sm:text-xl md:text-1xl font-small">
+      </h3>
+      
+      <p className="hidden text-neutral-400 sm:text-xl md:text-1xl font-small">
         SWE Club's mission is to foster a vibrant and inclusive community for computer science enthusiasts, providing opportunities to collaborate, learn, and gain practical experience in building real-world applications. 
         Through a fun and friendly environment, we aim to empower our members to excel in the competitive field of computer science.
-      </h3>
+      </p>
+
+      <p className="dark:block light:text-neutral-800 sm:text-xl md:text-1xl font-small">
+        SWE Club's mission is to foster a vibrant and inclusive community for computer science enthusiasts, providing opportunities to collaborate, learn, and gain practical experience in building real-world applications. 
+        Through a fun and friendly environment, we aim to empower our members to excel in the competitive field of computer science.
+      </p>
     </div>
   );
 }
