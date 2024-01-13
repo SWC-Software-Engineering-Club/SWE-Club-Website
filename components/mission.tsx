28c8b2ff4548ccdd82a305e8eceea2c8d70f76f8
Luis <104567400+Surdavow@users.noreply.github.com>
2024-01-13 13:25:52 -0800

    Clean up mission code
    The button component is still not updating dependent on the light or dark theme which is something that needs work before submitting a PR.

--- components/mission.tsx
+++ components/mission.tsx
@@ -1,19 +1,15 @@
-const Mission = () => {
+interface MissionProps {
+  darkMode: boolean;
+}
+
+const Mission: React.FC<MissionProps> = ({ darkMode }) => {
   return ( 
     <div className="max-w-3xl space-y-7 flex flex-col items-center mb-20 mt-20">
-      <img src="/icon-dark.svg" alt="Mission Icon" style={{ width: '10%' }} /> 
-      {/*<img className="light:block" src="/icon-light.svg" alt="Mission Icon" style={{ width: '10%' }} />*/}
+      <img src={darkMode ? "/icon-dark.svg" : "/icon-light.svg"} alt="Mission Icon" style={{ width: '10%' }} />
 
-      <h3 className="text-3xl sm:text-5xl md:text-3xl font-bold">
-        Our Mission
-      </h3>
+      <h3 className="text-3xl sm:text-5xl md:text-3xl font-bold">Our Mission</h3>
       
-      <p className="hidden text-neutral-400 sm:text-xl md:text-1xl font-small">
-        SWE Club's mission is to foster a vibrant and inclusive community for computer science enthusiasts, providing opportunities to collaborate, learn, and gain practical experience in building real-world applications. 
-        Through a fun and friendly environment, we aim to empower our members to excel in the competitive field of computer science.
-      </p>
-
-      <p className="dark:block light:text-neutral-800 sm:text-xl md:text-1xl font-small">
+      <p className={`${darkMode ? 'text-neutral-400' : 'text-neutral-1000'} sm:text-xl md:text-1xl font-small`}>
         SWE Club's mission is to foster a vibrant and inclusive community for computer science enthusiasts, providing opportunities to collaborate, learn, and gain practical experience in building real-world applications. 
         Through a fun and friendly environment, we aim to empower our members to excel in the competitive field of computer science.
       </p>
