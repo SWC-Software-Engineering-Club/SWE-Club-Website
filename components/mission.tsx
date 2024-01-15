7b93d2e88cc1f5e161f1801e944068147503711f
Luis <104567400+Surdavow@users.noreply.github.com>
2024-01-14 22:46:53 -0800

    Finalize mission component
    The margin issue is fixed and the text dynamically changes to darker or brighter dependent on the website theme. I removed the darkMode variable so that it doesn't cause issues anywhere else in the website.

--- components/mission.tsx
+++ components/mission.tsx
@@ -1,19 +1,15 @@
-interface MissionProps {
-  darkMode: boolean;
-}
-
-const Mission: React.FC<MissionProps> = ({ darkMode }) => {
+const Mission = () => {
   return ( 
-    <div className="max-w-3xl space-y-7 flex flex-col items-center mb-20 mt-20 relative">
-      <img src="/icon-light.svg" alt="Mission Icon" className={`hidden dark:block`}/>
-      <img src="/icon-dark.svg" alt="Mission Icon" className={`dark:hidden`}/>
-
-      <h3 className="text-3xl sm:text-5xl md:text-3xl font-bold">Our Mission</h3>
-      
-      <p className={`${darkMode ? 'text-neutral-400' : 'text-neutral-700'} sm:text-xl md:text-1xl font-small`}>
-        SWE Club's mission is to foster a vibrant and inclusive community for computer science enthusiasts, providing opportunities to collaborate, learn, and gain practical experience in building real-world applications. 
-        Through a fun and friendly environment, we aim to empower our members to excel in the competitive field of computer science.
-      </p>
+    <div className="flex flex-col items-center mt-10 mb-10 relative">
+      <img src="/icon-light.svg" alt="Mission Icon" className="absolute hidden dark:block"/>
+      <img src="/icon-dark.svg" alt="Mission Icon" className="absolute dark:hidden"/>        
+      <div className="max-w-3xl space-y-8 flex flex-col items-center mt-20 mb-10"> 
+        <h3 className="text-3xl sm:text-5xl md:text-3xl font-bold">Our Mission</h3>      
+        <p className="text-neutral-800 sm:text-xl md:text-1xl font-small mb-4 dark:text-neutral-600">
+          SWE Club's mission is to foster a vibrant and inclusive community for computer science enthusiasts, providing opportunities to collaborate, learn, and gain practical experience in building real-world applications. 
+          Through a fun and friendly environment, we aim to empower our members to excel in the competitive field of computer science.
+        </p>
+      </div>
     </div>
   );
 }
