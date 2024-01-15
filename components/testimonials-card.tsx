28dc4ac97ccfaeb8a26b44f90e8e601eeaf924ba
mccantsquincy <quincymccants@gmail.com>
2024-01-14 16:39:02 -0800

    added carousel to testimonial section.

--- components/testimonials-card.tsx
+++ components/testimonials-card.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { Avatar, AvatarImage } from "./ui/avatar";
+
 import {
   Card,
   CardContent,
@@ -5,49 +9,83 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Avatar, AvatarImage } from "./ui/avatar";
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 const clubTestimonials = [
-    {
-        name: "Luis",
-        role: "Developer",
-        testimonial: "Et obcaecati dignissimos a molestias minus eum labore laudantium aut atque nesciunt qui quasi galisum sit necessitatibus possimus"
-    },
-    {
-        name: "Adrian",
-        role: "Developer",
-        testimonial: "Et obcaecati dignissimos a molestias minus eum labore laudantium aut atque nesciunt qui quasi galisum sit necessitatibus possimus"
-    },
-    {
-        name: "Vinnie",
-        role: "Developer",
-        testimonial: "Et obcaecati dignissimos a molestias minus eum labore laudantium aut atque nesciunt qui quasi galisum sit necessitatibus possimus"
-    },
-
-]
+  {
+    name: "Luis",
+    role: "Developer",
+    testimonial:
+      "Et obcaecati dignissimos a molestias minus eum labore laudantium aut atque nesciunt qui quasi galisum sit necessitatibus possimus",
+  },
+  {
+    name: "Adrian",
+    role: "Developer",
+    testimonial:
+      "Et obcaecati dignissimos a molestias minus eum labore laudantium aut atque nesciunt qui quasi galisum sit necessitatibus possimus",
+  },
+  {
+    name: "Vinnie",
+    role: "Developer",
+    testimonial:
+      "Et obcaecati dignissimos a molestias minus eum labore laudantium aut atque nesciunt qui quasi galisum sit necessitatibus possimus",
+  },
+  {
+    name: "Anthony",
+    role: "Developer",
+    testimonial:
+      "Et obcaecati dignissimos a molestias minus eum labore laudantium aut atque nesciunt qui quasi galisum sit necessitatibus possimus",
+  },
+  {
+    name: "Kelvin",
+    role: "Developer",
+    testimonial:
+      "Et obcaecati dignissimos a molestias minus eum labore laudantium aut atque nesciunt qui quasi galisum sit necessitatibus possimus",
+  },
+];
 
 const TestimonialsCard = () => {
   return (
     <>
-    {clubTestimonials.map((member) => (
-       <div key={member.name}>
-      <Card className="flex flex-col items-start justify-star text-left">
-        <CardHeader>
-            <Avatar className="mb-2">
-            <AvatarImage src="https://github.com/shadcn.png" />
-            </Avatar>
-          <CardTitle >{member.name}</CardTitle>
-          <CardDescription className="text-bold text-neutral-400">{member.role}</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <p className="max-w-[400px]">{member.testimonial}</p>
-        </CardContent>
-      </Card>
-    </div> 
-    ))}
-     
+      <Carousel
+        opts={{
+          align: "start",
+        }}
+        className=""
+      >
+        <CarouselContent>
+          {clubTestimonials.map((member) => (
+            <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+              <div key={member.name}>
+                <Card className="flex flex-col items-start justify-start text-left">
+                  <CardHeader>
+                    <Avatar className="mb-2">
+                      <AvatarImage src="https://github.com/shadcn.png" />
+                    </Avatar>
+                    <CardTitle>{member.name}</CardTitle>
+                    <CardDescription className="text-bold text-neutral-400">
+                      {member.role}
+                    </CardDescription>
+                  </CardHeader>
+                  <CardContent>
+                    <p className="max-w-[400px]">{member.testimonial}</p>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </>
-    
   );
 };
 
--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -2,15 +2,12 @@ import TestimonialsCard from "./testimonials-card";
 
 const Testimonials = () => {
   return (
-    <div className="flex flex-col p-16">
+    <div className="flex flex-col m-16">
       <div className="p-12">
         <h3 className="text-3xl font-medium">Club Testimonials</h3>
       </div>
-      <div className="flex gap-9">
+      <div>
         <TestimonialsCard />
-      </div>
-      <div className="flex gap-4">
-        {/* <TestimonialsCard /> */}
       </div>
     </div>
   );
