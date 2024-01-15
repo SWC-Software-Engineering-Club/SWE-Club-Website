106c83783c592a11534965fb47cc1bc748c3ee2f
Luis <104567400+Surdavow@users.noreply.github.com>
2024-01-14 16:35:28 -0800

    Lower dark mode text

--- components/mission.tsx
+++ components/mission.tsx
@@ -9,7 +9,7 @@ const Mission: React.FC<MissionProps> = ({ darkMode }) => {
 
       <h3 className="text-3xl sm:text-5xl md:text-3xl font-bold">Our Mission</h3>
       
-      <p className={`${darkMode ? 'text-neutral-400' : 'text-neutral-1000'} sm:text-xl md:text-1xl font-small`}>
+      <p className={`${darkMode ? 'text-neutral-400' : 'text-neutral-600'} sm:text-xl md:text-1xl font-small`}>
         SWE Club's mission is to foster a vibrant and inclusive community for computer science enthusiasts, providing opportunities to collaborate, learn, and gain practical experience in building real-world applications. 
         Through a fun and friendly environment, we aim to empower our members to excel in the competitive field of computer science.
       </p>
