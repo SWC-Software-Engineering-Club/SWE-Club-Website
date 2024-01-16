928dc0cb47f1f065e35af9287c9d27e30b806fdb
mccantsquincy <quincymccants@gmail.com>
2024-01-15 18:37:45 -0800

    set up the layout for blogs page

--- components/blog-card.tsx
+++ components/blog-card.tsx
@@ -1,9 +1,40 @@
-const BlogCard = () => {
-    return ( 
-        <div>
-            Blog Card
-        </div>
-     );
+import Image from "next/image";
+import Link from "next/link";
+
+interface BlogCardProps {
+  blogContent: {
+    title: string;
+    image: string;
+    datePublished: string;
+  }[];
 }
- 
+
+const BlogCard = ({ blogContent }: BlogCardProps) => {
+  return (
+    <>
+      {blogContent?.map((content) => (
+        <Link href="/">
+          <div className="flex flex-col">
+            <Image
+              src={content.image}
+              alt="blog image"
+              width={500}
+              height={414}
+              className="mb-6"
+            />
+            <div>
+              <h4 className="text-lg">{content.title}</h4>
+            </div>
+            <div>
+              <span className="text-sm font-bold text-neutral-500">
+                {content.datePublished}
+              </span>
+            </div>
+          </div>
+        </Link>
+      ))}
+    </>
+  );
+};
+
 export default BlogCard;
