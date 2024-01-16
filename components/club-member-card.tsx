06246fbeba1333ea3194b61ff6a338c2f1fc1ba6
mccantsquincy <quincymccants@gmail.com>
2024-01-16 13:39:47 -0800

    implemented club member directory

---
+++ components/club-member-card.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+
+interface ClubMemberCardProps {
+  clubMemberContent: {
+    image: string;
+    name: string;
+    role: string;
+  }[];
+}
+
+const ClubMemberCard = ({ clubMemberContent }: ClubMemberCardProps) => {
+  return (
+    <>
+      {clubMemberContent?.map((member) => (
+        <div className="flex flex-col w-40 mb-16">
+          <Image 
+            src={member.image}
+            alt="member photo"
+            width={240}
+            height={240}
+            className="rounded-sm"
+          />
+          <div className="font-medium mt-4">{member.name}</div>
+          <div className="text-sm font-semibold text-neutral-500">{member.role}</div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default ClubMemberCard;
