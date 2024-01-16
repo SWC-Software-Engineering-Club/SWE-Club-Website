95b7fbfc8c5946aa16ade9ded8aac2cbe5d924ea
Luis <104567400+Surdavow@users.noreply.github.com>
2024-01-15 17:23:24 -0800

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -1,23 +1,18 @@
 import { Button } from "./ui/button";
 import Image from "next/image";
+
 const Header = () => {
   return (
     <>
-      <div className="absolute inset-0 bg-black opacity-50">
-        <div className="relative hero-content">
-          <Image
-            src="/hero-bg.png"
-            alt="black background"
-            //layout="fill"
-            className="absolute inset-0 w-full h-full"
-            width={4000}
-            height={2000}
-            objectFit="cover"
-          />
-        </div>
-      </div>
-
-      <div className="max-w-3xl space-y-7 flex flex-col items-center mb-28 mt-40">
+      <div className="relative space-y-7 flex flex-col items-center mb-20 mt-40 w-full">
+          <div className="absolute inset-0 w-full h-full">
+            <Image
+              src="/hero-bg.png"
+              alt="header background"
+              fill
+              style={{ objectFit: 'cover' }}
+            /> 
+          </div>
         <span>Software Engineering Club</span>
         <h1 className=" text-3xl sm:text-5xl md:text-6xl font-bold">
           Where CS Students of Southwestern College get ahead.
