09d9ab492e14a8e769b992ef47ec486e5edfe3f0
Luis <104567400+Surdavow@users.noreply.github.com>
2024-01-15 21:50:02 -0800

    Improve component

--- components/clubvalues.tsx
+++ components/clubvalues.tsx
@@ -1,15 +1,15 @@
 const ClubValues = () => {
   return ( 
-      <div> 
-      <h1 className="my-16 text-3xl font-medium text-justify">Club Values</h1>
-      <h2 className="mb-4 text-2xl font-regular text-justify">Skill Development</h2>
-      <p className="text-1xl font-regular text-justify">Provide members with opportunities to enhance their technical skills through hands-on projects, workshops, and coding challenges.</p>
-      <hr className="border-2 my-4 w-screen max-w-6xl"></hr>
-
-      
+    <div className="max-w-screen-lg"> 
+      <h1 className="my-28 text-3xl font-medium text-left">Club Values</h1>
+            
+      <div className="flex">
+        <h2 className="text-2xl text-left">Skill Development</h2>
+        <p className="text-2xl text-neutral-800 dark:text-neutral-600 ml-24">Provide members with opportunities to enhance their technical skills through hands-on projects, workshops, and coding challenges.</p>        
       </div>
+      <hr className="border-2 my-36"></hr>      
+    </div>
   );
 }
-
 
 export default ClubValues;
