e1423b4029d9fd91a9a5e832cc3b65e3fa2bdb8d
Luis <104567400+Surdavow@users.noreply.github.com>
2024-01-17 20:13:52 -0800

    Refactor and fix spacing issues

--- components/mission.tsx
+++ components/mission.tsx
@@ -1,15 +1,13 @@
 const Mission = () => {
   return ( 
-    <div id="mission-statement" className="flex flex-col items-center m-5 relative">
-      <img src="/icon-light.svg" alt="Mission Icon" className="absolute hidden dark:block"/>
-      <img src="/icon-dark.svg" alt="Mission Icon" className="absolute dark:hidden"/>        
-      <div className="max-w-3xl space-y-8 flex flex-col items-center mt-20 mb-10"> 
-      <h3 className="text-4xl font-bold">Our Mission</h3>    
-        <p className="text-neutral-800 sm:text-xl md:text-1xl font-small mb-4 dark:text-neutral-600">
+    <div id="mission-statement" className="flex flex-col items-center mt-24 mb-10 relative max-w-5xl">
+      <img src="/icon-light.svg" alt="Mission Icon" className="hidden dark:block"/>
+      <img src="/icon-dark.svg" alt="Mission Icon" className="dark:hidden"/>        
+      <h3 className="my-6 text-4xl font-bold">Our Mission</h3>    
+        <p className="text-neutral-800 dark:text-neutral-600 sm:text-xl md:text-1xl">
           SWE Club's mission is to foster a vibrant and inclusive community for computer science enthusiasts, providing opportunities to collaborate, learn, and gain practical experience in building real-world applications. 
           Through a fun and friendly environment, we aim to empower our members to excel in the competitive field of computer science.
         </p>
-      </div>
     </div>
   );
 }
