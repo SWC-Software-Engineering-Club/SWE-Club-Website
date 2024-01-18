905c94daaa139429969340285edf584b3e987442
Luis <104567400+Surdavow@users.noreply.github.com>
2024-01-17 22:02:16 -0800

    Initial implementation of footer

---
+++ components/footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+      <footer className="bg-gray-200 text-center text-xs p-3 fixed bottom-0 w-full border-t">
+        ©{new Date().getFullYear()} Southwestern College Software Engineering Club. All rights reserved.
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
