18e70ba91cc2174e67e6374b7bcb56b80dd31415
VinSevilla <vinsevilla24@gmail.com>
2024-01-18 16:02:14 -0800

    fix text layout

--- components/header.tsx
+++ components/header.tsx
@@ -6,25 +6,26 @@ const Header = () => {
     <>
       <div className="relative h-screen">
         <div className="max-w-full h-full space-y-7 flex flex-col items-center mb-28 mt-24 relative">
-          <Image
-            src="/hero-bg.png"
-            alt="black background"
-            layout="fill"
-            className="absolute inset-0"
-            objectFit="cover"
-          />
+          {/* Background Image Styling */}
+          <div className="absolute inset-0 bg-cover">
+            <Image
+              src="/hero-bg.png"
+              alt="black background"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
 
-          <span>Software Engineering Club</span>
-          <div className="flex flex-col items-center justify-center z-50 gap-y-6">
-            <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
+          {/* Text Box Content */}
+          <div className="flex flex-col items-center justify-center z-50 gap-y-6 mt-[50rem]">
+            <span>Software Engineering Club</span>
+            <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold mt-12 w-[46rem]">
               Where CS Students of Southwestern College get ahead.
             </h1>
-
-            <h3 className="text-base sm:text-xl md:text-2xl font-medium">
+            <h3 className="text-base sm:text-xl md:text-2xl font-medium w-[34rem]">
               Where SWC CS students can network, gain hands-on experience, and
               get ahead.
             </h3>
-
             <div className="flex gap-4">
               <Button>Apply</Button>
               <Button variant="outline">About Us</Button>
