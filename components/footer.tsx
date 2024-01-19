755ec926f9f63e8530a46d17ff3d3056be6a91de
mccantsquincy <quincymccants@gmail.com>
2024-01-19 11:29:02 -0800

    added bg gradient to footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,4 +1,5 @@
-// © {new Date().getFullYear()} Southwestern College Software Engineering Club. All rights reserved.
+import Image from "next/image";
+
 const FooterLinks = [
   {
     title: "Club Directory",
@@ -31,42 +32,55 @@ const FooterSocials = [
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col p-8">
-      <div className="pb-8">
-        <span className="font-bold text-neutral-500">SOUTHWESTERN COLLEGE</span>
-      </div>
-      <div>
-        <h2 className="text-5xl max-w-md font-bold pb-6">
-          Software Engineering Club
-        </h2>
-      </div>
-      <div>
-        <p className="text-xl max-w-xl pb-6">
-          Where CS Students of Southwestern College can get ahead by gaining
-          real world experience.
-        </p>
-      </div>
-      <div className="flex pb-24">
-        <div className="flex flex-col gap-y-4">
-          {FooterLinks.map((link) => (
-            <a key={link.title} href={link.link} className="pr-4">
-              {link.title}
-            </a>
-          ))}
+    <footer className="relative p-8">
+      <div className="flex flex-col !z-50">
+        <div className="pb-8">
+          <span className="font-bold text-neutral-600 dark:text-neutral-500">
+            SOUTHWESTERN COLLEGE
+          </span>
         </div>
-        <div className="flex flex-col gap-y-4 ml-24">
-          {FooterSocials.map((link) => (
-            <a key={link.title} href={link.link}>
-              {link.title}
-            </a>
-          ))}
+        <div>
+          <h2 className="text-5xl max-w-md font-bold pb-6">
+            Software Engineering Club
+          </h2>
         </div>
+        <div>
+          <p className="text-xl max-w-xl pb-6">
+            Where CS Students of Southwestern College can get ahead by gaining
+            real world experience.
+          </p>
+        </div>
+        <div className="flex pb-24">
+          <div className="flex flex-col gap-y-4">
+            {FooterLinks.map((link) => (
+              <a key={link.title} href={link.link} className="pr-4 font-semibold text-neutral-800 dark:text-neutral-200">
+                {link.title}
+              </a>
+            ))}
+          </div>
+          <div className="flex flex-col gap-y-4 ml-24">
+            {FooterSocials.map((link) => (
+              <a key={link.title} href={link.link} className="font-semibold text-neutral-800 dark:text-neutral-200">
+                {link.title}
+              </a>
+            ))}
+          </div>
+        </div>
+
+        <span>
+          © {new Date().getFullYear()} Southwestern College Software Engineering
+          Club. All rights reserved.
+        </span>
       </div>
 
-      <span>
-        © {new Date().getFullYear()} Southwestern College Software Engineering
-        Club. All rights reserved.
-      </span>
+      <Image
+        src="/bg-gradient-dark.png"
+        alt="background gradient"
+        width={2000}
+        height={2000}
+        className="absolute bottom-0 left-0 right-0"
+        style={{ zIndex: -1 }}
+      />
     </footer>
   );
 };
