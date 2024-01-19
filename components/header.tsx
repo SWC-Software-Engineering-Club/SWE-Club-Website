57734210b50bf42518f3b295d6cdc0bb556257d0
Luis <104567400+Surdavow@users.noreply.github.com>
2024-01-18 16:33:36 -0800

    Fix hero background component

--- components/header.tsx
+++ components/header.tsx
@@ -9,23 +9,28 @@ const Header = () => {
   };
 
   return (
-    <div className="flex flex-col items-center mb-24 mt-40">
-      <span className="text-base text-neutral-800 dark:text-neutral-400">Software Engineering Club</span>
-      <h1 className="text-6xl font-bold my-4 w-1/2 tracking-tight leading-tight">
-        Where CS Students of Southwestern College get ahead.
-      </h1>
-      <h3 className="text-xl text-neutral-800 dark:text-neutral-300 font-regular mb-8 w-1/3">
-        Where SWC CS students can network, gain hands-on experience, and get
-        ahead.
-      </h3>
-      <div className="flex gap-4">
-        <Button>Apply</Button>
-
-        <Button variant="outline" onClick={handleClick}>
-          About Us
-        </Button>
-      </div>
-    </div>
+      <>
+        <div className="z-50 flex flex-col items-center mb-24 mt-40">           
+          <span className="text-base text-neutral-800 dark:text-neutral-400">Software Engineering Club</span>
+          <h1 className="text-6xl font-bold my-4 w-1/2 tracking-tight leading-tight">
+            Where CS Students of Southwestern College get ahead.
+          </h1>
+          <h3 className="text-xl text-neutral-800 dark:text-neutral-300 font-regular mb-8 w-1/3">
+            Where SWC CS students can network, gain hands-on experience, and get
+            ahead.
+          </h3>
+            <div className="flex gap-4">
+              <Button>Apply</Button>
+              <Button variant="outline" onClick={handleClick}>About Us</Button>
+            </div>         
+        </div>
+          <Image
+          src="/hero-bg.png"
+          alt="black background"
+          layout="fill"
+          objectFit="cover"
+          />      
+      </>
   );
 };
 
