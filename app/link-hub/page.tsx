e16b8d60c623c10bfe46d01d09803a55b3ad95a2
mccantsquincy <quincymccants@gmail.com>
2024-01-23 16:55:48 -0800

    adjusted footer tag padding

--- app/link-hub/page.tsx
+++ app/link-hub/page.tsx
@@ -4,9 +4,9 @@ import LinkHubUi from "./_component/link-hub-ui";
 
 const LinkHubPage = () => {
   return (
-    <div className="relative mt-20 md:mt-40 flex flex-col items-center px-6 h-screen">
+    <div className="relative mt-20 md:mt-40 flex flex-col items-center h-screen">
       <LinkHubUi />
-      <div className="absolute bottom-0 text-xs text-center mb-28">
+      <div className="absolute bottom-0 text-xs text-center mb-28 px-6">
         <span>
         © {new Date().getFullYear()} Southwestern College Software Engineering
         Club. All rights reserved.
